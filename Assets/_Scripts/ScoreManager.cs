0f398b8180c4cb249f2ca2c13458ef92501d2694
NazmulHridoy <30873112+NazmulHridoy@users.noreply.github.com>
2021-01-26 19:37:07 +0600

    Update ScoreManager.cs

--- Assets/_Scripts/ScoreManager.cs
+++ Assets/_Scripts/ScoreManager.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int score = 0;
+    #region Singleton
+    private static ScoreManager instance;
+    public static ScoreManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
